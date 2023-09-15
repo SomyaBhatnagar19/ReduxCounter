057f96e74386d85ee7ba840b4d49621ee4f2cc95
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-09-15 10:55:25 +0530

    Making and installing a new setup to make proper counter as the instructor.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,7 +6,9 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
+      <hr className={classes.hrLine}></hr>
       <div className={classes.value}>-- COUNTER VALUE --</div>
+      <hr className={classes.hrLine}></hr>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
