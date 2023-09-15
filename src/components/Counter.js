668e4d5d4e6ecac53ac0243fdb8c2982e1c0ae58
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-09-15 12:34:13 +0530

    Making the functionality to increment and decrement when the user clicks the buttons.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,33 @@
+import { useSelector, useDispatch } from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const dispatch = useDispatch();
+
+  const counter = useSelector(state => state.counter);
+  
+  const incrementHandler = () => {
+    dispatch({type: 'increment'});
+  }
+
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement'});
+  }
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <hr className={classes.hrLine}></hr>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <hr className={classes.hrLine}></hr>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <div className={classes.buttons}>
+  <button onClick={incrementHandler}>Increment</button>
+  <button onClick={toggleCounterHandler}>Toggle Counter</button>
+  <button onClick={decrementHandler}>Decrement</button>
+</div>
+      
     </main>
   );
 };
