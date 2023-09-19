de1d071dd1b9c0bc9caca9ddf8a6b1626fb83ab3
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-09-19 11:54:09 +0530

    Logout functionality using redux.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import { UseSelector, useSelector } from 'react-redux/es/hooks/useSelector';
-
+import { useDispatch } from 'react-redux';
 import classes from './Header.module.css';
+import { authActions } from '../store/index';
 
 const Header = () => {
+  const dispatch = useDispatch();
   const isAuth = useSelector(state => state.auth.isAuthenticated);
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout())
+  }
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
@@ -16,7 +23,7 @@ const Header = () => {
             <a href='/'>My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            <button onClick={logoutHandler}>Logout</button>
           </li>
         </ul>
       </nav>}
